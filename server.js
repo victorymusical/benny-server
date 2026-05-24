47986f3ff6272f91f701f4188097443e8c6ba239
victorymusical <melvin@victorymusical.com>
2026-05-24 10:22:16 -0400

    Add debug routes to the server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import chatRoutes from "./routes/chat.js";
 import path from "path";
+import debugRoutes from "./routes/debug.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/chat", chatRoutes);
 app.use(express.static("public"));
+app.use("/api/debug", debugRoutes);
 
 app.get("/", (req, res) => {
   res.json({
