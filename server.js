f29e6c12236a59c4591fbbd2da8c76536c8db754
victorymusical <melvin@victorymusical.com>
2026-05-23 22:29:38 -0400

    Enable serving static files from 'public' directory
    Add static file serving from the 'public' directory.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import chatRoutes from "./routes/chat.js";
+import path from "path";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/chat", chatRoutes);
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.json({
