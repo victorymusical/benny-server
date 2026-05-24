ab024bb9ff067019157bbfdd448663296f5c562a
victorymusical <melvin@victorymusical.com>
2026-05-23 22:10:55 -0400

    Add chat routes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import chatRoutes from "./routes/chat.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
   res.json({
